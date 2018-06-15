b79e479c77fde61ad1241249ae4c796603c50c7b
luatnguyen-hcmus <luatnguyen13ck4@gmail.com>
2018-06-15 18:33:37 +0700

    change index

--- index.php
+++ index.php
@@ -30,8 +30,8 @@
  * @author	EllisLab Dev Team
  * @copyright	Copyright (c) 2008 - 2014, EllisLab, Inc. (https://ellislab.com/)
  * @copyright	Copyright (c) 2014 - 2018, British Columbia Institute of Technology (http://bcit.ca/)
- * @license	http://opensource.org/licenses/MIT	MIT License
- * @link	https://codeigniter.com     
+ * @license	http://opensource.org/licenses/MIT	    MIT License
+ * @link	https://codeigniter.com         
  * @since	Version 1.0.0
  * @filesource
  */
